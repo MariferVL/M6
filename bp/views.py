ac2b67f8737e66fefd3a9f7261711ee61836ac29
unknown <mfvl.1186@gmail.com>
2022-04-11 21:15:16 -0400

    post in html

--- bp/views.py
+++ bp/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Post
+
 
 # Create your views here.
 
-def home_list(request):
-    return render(request, 'bp/home_list.html', {})
+def home(request):
+    posts = Post.objects.order_by('published_date')
+    return render(request, 'bp/home.html', {'posts': posts})
