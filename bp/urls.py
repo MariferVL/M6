4ca4684628800cb52c4ba811cbe67f81167047ae
unknown <mfvl.1186@gmail.com>
2022-04-12 13:27:28 -0400

    post html ready

--- bp/urls.py
+++ bp/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name= 'home'),
+    path('post/<int:pk>/',views.post_detail, name='post_detail'),
 ]
--- bp/views.py
+++ bp/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from .models import Post
 
@@ -8,3 +8,7 @@ from .models import Post
 def home(request):
     posts = Post.objects.order_by('published_date')
     return render(request, 'bp/home.html', {'posts': posts})
+
+def post_detail(request,pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'bp/post_detail.html', {'post': post})
