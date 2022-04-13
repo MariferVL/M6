e92bdb8b1c332824507b258281f8380c24a51706
unknown <mfvl.1186@gmail.com>
2022-04-12 21:19:15 -0400

    edit added

--- bp/urls.py
+++ bp/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.home, name= 'home'),
     path('post/<int:pk>/',views.post_detail, name='post_detail'),
+    path('post/new/', views.post_new, name='post_new'),
+    path('post/<int:pk>/edit/', views.post_edit, name='post_edit'),
 ]
